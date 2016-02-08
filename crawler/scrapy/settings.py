63e59a0fefa4ec69f26c66f1b329c4371c039f1e
Debjyoti Paul <debjyotipaul385@gmail.com>
2016-02-08 04:05:00 -0700

    added elasticsearch config

--- crawler/scrapy/settings.py
+++ crawler/scrapy/settings.py
@@ -7,6 +7,7 @@
 #
 #     http://doc.scrapy.org/en/latest/topics/settings.html
 #
+from scrapy import log
 
 BOT_NAME = 'NYtimes'
 
@@ -15,6 +16,17 @@ NEWSPIDER_MODULE = 'NYtimes.spiders'
 ITEM_PIPELINES = {
 	'NYtimes.pipelines.NYtimesPipeline' : 500,
 }
+ITEM_PIPELINES = [
+            'scrapyelasticsearch.scrapyelasticsearch.ElasticSearchPipeline',
+            ]
 
+ELASTICSEARCH_SERVER = 'localhost' # If not 'localhost' prepend 'http://'
+ELASTICSEARCH_PORT = 9200 # If port 80 leave blank
+ELASTICSEARCH_USERNAME = ''
+ELASTICSEARCH_PASSWORD = ''
+ELASTICSEARCH_INDEX = 'news'
+ELASTICSEARCH_TYPE = 'articles'
+ELASTICSEARCH_UNIQ_KEY = 'url'  # Custom uniqe key like 'student_id'
+ELASTICSEARCH_LOG_LEVEL= log.DEBUG
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'NYtimes (+http://www.yourdomain.com)'
--- requirements.sh
+++ requirements.sh
@@ -1,2 +1,6 @@
+sudo apt-get install libffi-dev
+sudo apt-get install libssl-dev
+sudo apt-get install libxml2-dev libxslt1-dev python-dev
+
 pip install scrapy
 pip install pyelasticsearch
