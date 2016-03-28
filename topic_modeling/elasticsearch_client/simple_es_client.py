2b9f6115f15b060e1b5ec1f83a8f5c0e70a673fc
Debjyoti Paul <debjyotipaul385@gmail.com>
2016-03-28 02:03:18 -0600

    modifying es client code

--- topic_modeling/elasticsearch_client/simple_es_client.py
+++ topic_modeling/elasticsearch_client/simple_es_client.py
@@ -3,31 +3,40 @@ from elasticsearch import Elasticsearch
 
 es = Elasticsearch(hosts=[{'host':'db03.cs.utah.edu'},{'host':'db04.cs.utah.edu'}])
 
-result_set = es.search(index="news",body={
-"query": {
-   "bool": {
-        "must": [
-               { "match": { "date": "2016-02-3" } }
-                      
-                           ]
-                              }
-                               },
-                                "sort" : 
-                                       { "date" : {"order" : "desc"}},
-                                              
-                                               "size":50000,
-                                                "from":0
-                                                
-                                                })
-i=0                                                
-with open('articles3.txt', 'w') as f:
-    with  open('articles2.txt', 'w') as f1:
-        for  hit in result_set['hits']['hits']:
-            i = i+1
-            print i
-            c =  hit['_source']
-            f.write((c['article'] + c['keywords'] +c['focus']).encode('utf-8').replace('\n',' '))
-            f1.write((c['title'] + ". " +  c['keywords']).encode('utf-8').replace('\n',' '))
-            f.write("\n")
-            f1.write("\n")
+def create_lda_input_files(size=5000, filename="articles.txt"):
+    from_value=0
+    total=1
+    i=0                                                
+    while total > from_value:
+        result_set = es.search(index="news",body={
+        "query": {
+           "bool": {
+                "must": [
+                       #{ "match": { "month": "2" } },
+                       {"match": {"year":2016}}
+                              
+                                   ]
+                                      }
+                                       },
+                                        "sort" : 
+                                               { "date" : {"order" : "desc"}},
+                                                      
+                                                       "size":5000,
+                                                        "from":from_value
+                                                        
+                                                        })
+        total=result_set['hits']['total']
+        from_value=from_value + size
+        with open(filename, 'a') as f:
+            with  open('articles2.txt', 'a') as f1:
+                for  hit in result_set['hits']['hits']:
+                    i = i+1
+                    print i, hit['origin'], hit['date']
+                    c =  hit['_source']
+                    f.write((c['article'] + c['keywords'] +c['focus']).encode('utf-8').replace('\n',' '))
+                    f1.write((c['title'] + ". " +  c['keywords']).encode('utf-8').replace('\n',' '))
+                    f.write("\n")
+                    f1.write("\n")
 
+
+create_lda_input_files()
