7bcce775579d852bf9f089dc5d0c9c0a8ac45619
Debjyoti Paul <debjyotipaul385@gmail.com>
2016-04-11 03:47:05 -0600

    adding insert events to es

--- clustering/insert_events.py
+++ clustering/insert_events.py
@@ -9,7 +9,9 @@ es = Elasticsearch(hosts=[{'host':'db03.cs.utah.edu'}])
    
 def upsert_event(Client, event):
     id = event['id']
-    Client.update(index="testevents", doc_type="news", id=id, body={"doc":json.dumps(event),'doc_as_upsert':True})
+    Client.update(index="testevents", doc_type="news", id=id, body={"doc":event,'doc_as_upsert':True})
 
 if __name__=="__main__":
-    updateES(ESClient=es)
+    event={}
+    event['id']= "1"
+    upsert_event(es, event)
