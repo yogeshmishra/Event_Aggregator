5a0d57b63d86a8fcc68bba53fa30d998456d213c
Debjyoti Paul <debjyotipaul385@gmail.com>
2016-04-11 04:42:13 -0600

    adding insert events to es

--- clustering/insert_events.py
+++ clustering/insert_events.py
@@ -8,6 +8,33 @@ es = Elasticsearch(hosts=[{'host':'db03.cs.utah.edu'}])
 
    
 def upsert_event(Client, event):
+    """
+        structure of event        
+        {
+          "id": "e17f6d76897496f2911726401e9efecd72d2da57",
+          "title":"Title comes here",
+          "articles":[
+              {
+               "id": "id1",
+               "link": "http://economictimes.com/adjaccaasd",
+               "title": "articles title"
+              },
+              {
+               "id": "id2",
+               "link": "http://economictimes.com/adjdaasdqa",
+               "title": "articles title"
+              }
+            ],
+            "date":"2016-01-01",
+            "year":2016,
+            "month" : 1,
+            "day": 1,
+            "keywords": "key1,key2,key3",
+            "location": "loc1,loc2,loc3",
+            "category": "category name"
+        }
+
+    """
     id = event['id']
     Client.update(index="testevents", doc_type="news", id=id, body={"doc":event,'doc_as_upsert':True})
 
