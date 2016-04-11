f2c57ad226da614e279022a1b41b3bf89ff8cb95
yogeshmishra <yogeshmishra.india@gmail.com>
2016-04-10 23:46:32 -0600

    Bag of words

--- clustering/tf-idf.py
+++ clustering/tf-idf.py
@@ -3,23 +3,19 @@ import sys
 import ast
 
 baseFolder="../data/"
-def createText(file):
-    text =[]
-    with open(baseFolder+"non_ners.txt", 'r')  as non_ners:
-        for line in  non_ners:
-            line = ast.literal_eval(line)
-            text.append(line)
-    return text
 
-def MyCorpus(dictionary):
-    for line in open(baseFolder+'non_ners.txt'):
+fileList = ['non_ners.txt', 'locations.txt', 'persons.txt', 'enriched_keys.txt']
+
+def MyCorpus(dictionaryi, fileName):
+    for line in open(baseFolder+filename):
         # assume there's one document per line, tokens separated by whitespace
         yield dictionary.doc2bow(ast.literal_eval(line))
 
 
-dictionary = corpora.Dictionary( ast.literal_eval(line) for line in open(baseFolder+'non_ners.txt'))
-dictionary.save('/tmp/news_articles.dict') # store the dictionary, for future reference
-print dictionary
+for filename in fileList:
+    dictionary = corpora.Dictionary( ast.literal_eval(line) for line in open(baseFolder+ filename))
+    dictionary.save('/tmp/' + filename.strip('.txt') +'.dict') # store the dictionary, for future reference
+    print dictionary
 
-corpus_memory_friendly = MyCorpus(dictionary)
-corpora.MmCorpus.serialize('/tmp/news_articles.mm', corpus_memory_friendly)
+    corpus = MyCorpus(dictionary, filename)
+    corpora.MmCorpus.serialize('/tmp/' + filename.strip('.txt') +'.mm', corpus)
