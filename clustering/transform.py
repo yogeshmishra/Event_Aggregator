61a8795593c653556868c22730138ed213b4fe87
Debjyoti Paul <debjyotipaul385@gmail.com>
2016-04-11 03:27:39 -0600

    changed transform

--- clustering/transform.py
+++ clustering/transform.py
@@ -21,15 +21,11 @@ fileListRemoved = [ 'locations.txt', 'persons.txt', 'enriched_keys.txt']
 
 mainFile = 'non_ners.txt' 
 results = [[ 0.0 for i in range(len(sims[mainFile][0]))] for j in range(len(sims[mainFile]))]
-print results
+#print results
 for key in fileList:
     sim = sims[key]
-    #print key
     for i in range(len(sim)):
-        #print sims[mainFile][i][:5], sim[i][:5]
-        #print  [ x + y for x,y in zip(results[i],sim[i])][:5]
         results[i] = [ x + y for x,y in zip(results[i],sim[i])]
-        #print results[i]
 aggSim = results
 
 with open("output.txt","w") as out:
